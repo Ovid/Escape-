8d0facbd26eac3332171a944650f6c9f82d50328
Curtis Poe <ovid@cpan.org>
2009-08-16 16:22:08 +0100

    Add the database.

---
+++ db/escape.sql
@@ -0,0 +1,9 @@
+CREATE TABLE country (
+    id         INTEGER PRIMARY KEY,
+    iso        CHAR(2)      NOT NULL,
+    url_key    VARCHAR(255) NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    population INTEGER          NULL,
+    capital    VARCHAR(255) NOT NULL,
+    wikipedia  VARCHAR(255) NOT NULL
+);
