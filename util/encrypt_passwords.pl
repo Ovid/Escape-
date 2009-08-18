ba700d086514a7495e5b25c0cc7990ca1d54d226
Curtis Poe <ovid@cpan.org>
2009-08-18 21:19:18 +0100

    Add password encryption utility.

---
+++ util/encrypt_passwords.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Escape::Schema;
+
+my $schema = Escape::Schema->connect('dbi:SQLite:db/escape.db');
+
+my @users = $schema->resultset('User')->all;
+
+foreach my $user (@users) {
+    print $user->password, $/;
+    next;
+    $user->password($user->password);
+    $user->update;
+}
