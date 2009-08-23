0cb736aea04c31f5ecbc31de366ae489d45bf275
Curtis Poe <ovid@cpan.org>
2009-08-23 17:03:38 +0100

    Have a simple (brain-dead) test database handy.

---
+++ t/lib/TestDB.pm
@@ -0,0 +1,68 @@
+package TestDB;
+
+use strict;
+use warnings;
+
+use DBI;
+
+use base 'Exporter';
+our @EXPORT_OK = qw(
+  model
+  dbi
+  dsn
+);
+
+=head1 NAME
+
+TestDB - Quick hack to override the database
+
+=head1 SYNOPSIS
+
+ use TestDB;
+ use Test::WWW::Mechanize::Catalyst 'Escape';
+
+The test database I<must> be loaded before Escape is.
+
+=cut
+
+use App::Info::RDBMS::SQLite;
+use File::Temp 'tempfile';
+
+sub _confess($) {
+    require Carp;
+    Carp::confess shift;
+}
+
+BEGIN {
+    if ( exists $INC{'Escape/Model/DB/pm'} ) {
+        _confess __PACKAGE__ . ". must be loaded prior to Escape::Model::DB";
+    }
+
+    my $sqlite = App::Info::RDBMS::SQLite->new;
+    if ( not $sqlite->installed ) {
+        _confess "App::Info::RDBMS::SQLite does not think SQLite is installed";
+    }
+
+    if ( $sqlite->major_version < 3 ) {
+        my $version = $sqlite->version;
+        _confess "You need SQLite version 3 or better, not '$version'";
+    }
+
+    my $executable = $sqlite->executable;
+    my $schema     = 'db/full_schema.sql';
+    my ( undef, $filename ) = tempfile();
+    system("$executable $filename < $schema") == 0
+      or _confess "Cannot load $schema into $filename: $?";
+
+    my $dsn = $ENV{ESCAPE_TEST_DSN} = "dbi:SQLite:$filename";
+    sub dsn { $dsn }
+
+    my $dbh = DBI->connect($dsn);
+    sub dbh { $dbh }
+}
+
+use Escape;
+my $model = Escape->new->model('DB');
+sub model { $model }
+
+1;
