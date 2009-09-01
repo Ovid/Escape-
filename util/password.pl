ff53263f3303beb62c43934f398a98c7943e293c
Curtis Poe <ovid@cpan.org>
2009-09-01 09:10:26 -0700

    Utility to update user passwords.

---
+++ util/password.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use IO::Prompt;
+
+use Escape::Schema;
+
+my $username = shift || die "No user supplied";
+my $schema   = Escape::Schema->connect('dbi:SQLite:db/escape.db');
+my @user     = $schema->resultset('User')->search( { username => $username } );
+
+die "No such user ($username)" unless @user;
+die "PANIC: Multiple users found for ($username)" if @user > 1;
+
+my $pass1 = prompt( "Enter new password: ", -echo => '*' );
+my $pass2 = prompt( "Confirm password:   ", -echo => '*' );
+
+unless ( $pass1 eq $pass2 ) {
+    die "Entered passwords don't match";
+}
+$user[0]->password($pass1);
+$user[0]->update;
+print "Password updated\n";
